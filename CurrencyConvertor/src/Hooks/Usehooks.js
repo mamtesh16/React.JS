559aa352de0c80b3f9ea6082cdc1ce5cf3636207
mamtesh kumar <mamteshk8612gmail.com>
2026-09-17 15:20:05 +0530

    change

--- CurrencyConvertor/src/Hooks/Usehooks.js
+++ CurrencyConvertor/src/Hooks/Usehooks.js
@@ -3,13 +3,16 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
     const [data, setData] = useState({});
 
-    // kuch karwana hai isliye useEffect
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(
+            `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json`
+        )
             .then((res) => res.json())
-            .then((res) => setData(res[currency]));
+            .then((res) => setData(res[currency]))
+            .catch((error) => console.log(error));
     }, [currency]);
-//[currency]  jb currency me change ho tb call kare
+
     return data;
 }
+
 export default useCurrencyInfo;
